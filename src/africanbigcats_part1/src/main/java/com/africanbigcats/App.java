351ef215e6145f88618b8b5af12188c590f5a1cf
vinhmech <vincenguyen262@gmail.com>
2021-08-23 21:06:15 -0700

    create African Big Cat project

---
+++ src/africanbigcats_part1/src/main/java/com/africanbigcats/App.java
@@ -0,0 +1,38 @@
+package com.africanbigcats;
+
+import java.util.*;
+
+/**
+ * African Big Cat Main File
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        Menu appMenu = new Menu();
+
+        Character command = '_';
+        LinkedList<Panthera> catList = new LinkedList<>(); 
+
+        // loop until user quits
+        while (command != 'q') {
+
+            // print the menu
+            appMenu.print();
+
+            // get a command
+            System.out.print("Enter a command: ");
+            command = appMenu.getCommand();
+
+            // execute a commmand
+            appMenu.executeCommand(command, catList);
+
+            // move the cats to new positions in africa
+            appMenu.update(catList);
+
+        }
+
+    }
+}
